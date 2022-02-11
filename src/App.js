b897237c8ae9cc1e75ce6ff0aef938150cd67525
Christinalytle <christina.lytle17@gmail.com>
2022-02-10 20:06:49 -0700

    first push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header.js'
+import MemeGenerator from './MemeGenerator.js'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+     
+    </div>
+  );
+}
+
+export default App;
